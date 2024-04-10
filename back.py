ced32c171c4408b65f526af4a14927ee2ed5dc66
POOJA HARIHAR <88924201+poojaharihar03@users.noreply.github.com>
2024-04-10 23:11:25 +0530

    Update back.py

--- back.py
+++ back.py
@@ -20,8 +20,7 @@ from pydub.playback import play
 import toml
 
 hf_token = os.getenv("HUGGINGFACE_TOKEN")
-print(hf_token)
-#os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_token
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = hf_token
 # if hf_token is None:
 #     raise ValueError("Hugging Face token is not set. Please set it in Streamlit settings.")
 # else:
