654b78061bab7c29b71668c9732060dc1ed9cfae
POOJA HARIHAR <88924201+poojaharihar03@users.noreply.github.com>
2024-05-26 09:49:16 +0530

    Update front.py

--- front.py
+++ front.py
@@ -14,15 +14,16 @@ with st.sidebar:
 
     st.markdown(" - Users can adjust token length to control the length of generated responses, allowing for customization based on specific requirements or constraints.")
     st.markdown(" - Users can adjust the temp to control response randomness. Higher values (e.g., 0.5) produce diverse but less focused responses, while low values (e.g., 0.1) result in more focused but less varied answers.")
-    st.markdown("- Users can now use text to speech")
+    st.markdown("- users can now use text to speech")
+    st.markdown("<h2 style='font-family:Georgia;font-size:20px;'>Press enter to stop the audio</h1>", unsafe_allow_html=True)
 
     st.markdown("-------")
     st.markdown("<h2 style='text-align:center;font-family:Georgia;font-size:20px;'>Advanced Features</h1>",
                 unsafe_allow_html=True)
-    max_length = st.slider("Token Max Length", min_value=128, max_value=4096, value=1024, step=128)
+    max_length = st.slider("Token Max Length", min_value=128, max_value=3000, value=512, step=128)
     temp = st.slider("Temperature", min_value=0.1, max_value=1.0, value=0.5, step=0.1)
 
-if "messages" not in st.session_state.keys():
+if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
 # Display or clear chat messages
@@ -34,43 +35,31 @@ def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-def submit():
-    st.session_state.something = st.session_state.widget
-    st.empty()
-
-if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "How may I help you today?"}]
-
 if user_prompt := st.chat_input("enter your query"):
     st.session_state.messages.append({"role": "user", "content": user_prompt})
     with st.chat_message("user"):
         st.write(user_prompt)
 
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = model(user_prompt, max_length, temp)
-            placeholder = st.empty()
-            full_response = ''
-            for item in response:
-                full_response += item
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = model(user_prompt, max_length, temp)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response:
+                    full_response += item
+                    placeholder.markdown(full_response)
                 placeholder.markdown(full_response)
-            placeholder.markdown(full_response)
-    message = {"role": "assistant", "content": full_response}
-    st.session_state.messages.append(message)
+        message = {"role": "assistant", "content": full_response}
+        st.session_state.messages.append(message)
 
 if st.button("Convert to Speech"):
     if st.session_state.messages[-1]["role"] == "assistant":
         audio_base64 = text_speech(st.session_state.messages[-1]["content"])
         st.audio(base64.b64decode(audio_base64), format='audio/mp3')
-# if st.button("Convert to Speech"):
-#         if st.session_state.messages[-1]["role"] == "assistant":
-#             text_speech(st.session_state.messages[-1]["content"])
-
 
 st.markdown("""
     <div style="background-color: #FFFF8F	; padding: 10px; border-radius: 5px;">
         <p style="color: black;">⚠️Senor can make mistakes. Information updated only till 2023. Please consider checking important information.</p>
     </div>
 """, unsafe_allow_html=True)
-
